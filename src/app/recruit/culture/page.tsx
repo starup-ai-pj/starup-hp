a426a16f03088119fe61bcd922675a47b36a9fa9
ShotaGhoona <shota.yamashita@ghoona.com>
2026-04-06 17:40:16 +0900

    feat: Cultureページ新規作成 (#26)

---
+++ src/app/recruit/culture/page.tsx
@@ -0,0 +1,13 @@
+import Header from '@/components/layout/Header'
+import Footer from '@/components/layout/Footer'
+import CulturePage from '@/components/sections/culture/CulturePage'
+
+export default function RecruitCulturePage() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <CulturePage />
+      <Footer />
+    </div>
+  )
+}
